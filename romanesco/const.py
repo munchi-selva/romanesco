0499ba8e2d60bffcf609c504ff2ee942f516b36d
munchi-selva <munchi@gmail.com>
2019-04-22 08:41:38 +0000

    Minor correction to tokenization

--- romanesco/const.py
+++ romanesco/const.py
@@ -2,12 +2,13 @@
 
 EOS = '<eos>'
 UNK = '<unk>'
-TOKEN_PATTERN = '[,“.”?!;—]|[^,“.”?!;\s—]+' # Customized to Shakespearean text
+TOKEN_PATTERN = '[,;:!?.“”()—]|[^,;:!?.“”()—\s]+'   # Customized to Shakespearean text
 
 MODEL_FILENAME = 'model'
 VOCAB_FILENAME = 'vocab.json'
 
 # Ugly hardcoded hyperparameters
-NUM_STEPS = 35 # truncated backprop length
+NUM_STEPS = 35 # truncated backprop length 
+# Note: simple experiments suggest NUM_STEPS = 25 produces better results
 LEARNING_RATE = 0.0001
 HIDDEN_SIZE = 1500 # layer size

5753934c460eab34c4415b2d6b0aea42e2ce914d
munchi-selva <munchi@gmail.com>
2019-04-27 18:41:30 +0000

    Update defaults to reflect optimum Shakespearean settings

--- romanesco/const.py
+++ romanesco/const.py
@@ -18,16 +18,16 @@ VOCAB_FILENAME = 'vocab.json'
 CONFIG_FILENAME = 'config.json'
 
 VOCAB_SIZE = 10000
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
-NUM_EPOCHS = 10
+NUM_EPOCHS = 50
 VAL_EPOCHS = 1
 PATIENCE = 5
 
-# num_steps and learning_rate are hardcoded here; at the moment,
-# the only way to change them is to edit this file
 NUM_STEPS = 100 # truncated backprop length
-                # formerly 35, experiments with Shakespeare functioned better with 25
+
+# learning_rate is hardcoded here; at the moment,
+# the only way to change it is to edit this file
 LEARNING_RATE = 0.001   # formerly 0.0001
 
 HIDDEN_SIZE = 1024  # RNN state size, formerly 1500
